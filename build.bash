6d1af6fc0be8ae74fb9c8c351d112c4dd2b873e2
Freedom645 <16729421+Freedom645@users.noreply.github.com>
2025-10-08 17:57:06 +0900

    ビルドスクリプトの修正

--- build.bash
+++ build.bash
@@ -6,5 +6,3 @@ uv run pyinstaller replacer.spec --noconfirm
 
 mkdir -p dist/InfTimeStamper
 mv dist/replacer.exe dist/InfTimeStamper/
-
-powershell -c Compress-Archive -Path "dist/InfTimeStamper/*" -DestinationPath dist/InfTimeStamper.zip -Force
